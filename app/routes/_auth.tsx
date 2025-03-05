031ccbfc5ebd42a41fca4a3200dfbb3a73175aff
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-05 22:54:30 +0530

    changed theme

--- app/routes/_auth.tsx
+++ app/routes/_auth.tsx
@@ -1,4 +1,5 @@
 import { Link, Outlet } from "@remix-run/react";
+import ThemeToggle from "~/component/ThemeToggle";
 
 export default function AuthLayout() {
   return (
@@ -12,8 +13,13 @@ export default function AuthLayout() {
         </h1>
         <div className="absolute inset-0 nav-design-container bg-base-200 -z-10"></div>
       </div>
-      <div className="max-w-[90vw] w-96 mx-auto flex flex-col items-center justify-center relative">
-        <Outlet />
+      <div className="flex flex-col items-center justify-center relative">
+        <div className="absolute right-5 top-5">
+          <ThemeToggle/>
+        </div>
+        <div className="max-w-[90vw] w-80 mx-auto ">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
