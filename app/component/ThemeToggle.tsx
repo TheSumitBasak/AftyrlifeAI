5997be97f2574f41a5b47b1027ae193da8a05050
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-06 22:11:27 +0530

    added tooltip

--- app/component/ThemeToggle.tsx
+++ app/component/ThemeToggle.tsx
@@ -2,18 +2,27 @@ import { Moon, Sun } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function ThemeToggle() {
-  const [theme, setTheme] = useState<string>(() => {
-    // Initialize theme from localStorage, default to 'winter'
-    return globalThis?.localStorage?.getItem?.("site-theme") || "winter";
-  });
+  const [theme, setTheme] = useState<string>("");
 
   // Themes array for easy switching
   const themes = ["winter", "night"];
 
   useEffect(() => {
     // Update localStorage and html attribute when theme changes
-    globalThis?.localStorage?.setItem?.("site-theme", theme);
-    document.documentElement.setAttribute("data-theme", theme);
+    if (theme) {
+      globalThis?.localStorage?.setItem?.("site-theme", theme);
+      document.documentElement.setAttribute("data-theme", theme);
+    } else {
+      const isDarkMode =
+        globalThis?.window?.matchMedia &&
+        globalThis?.window?.matchMedia?.("(prefers-color-scheme: dark)")
+          ?.matches;
+
+      const theme =
+        globalThis?.localStorage?.getItem?.("site-theme") ||
+        (isDarkMode ? "night" : "winter");
+      setTheme(theme);
+    }
   }, [theme]);
 
   const toggleTheme = () => {
@@ -23,16 +32,18 @@ export default function ThemeToggle() {
   };
 
   return (
-    <button
-      onClick={toggleTheme}
-      className="btn btn-ghost btn-square"
-      aria-label="Toggle Theme"
-    >
-      {theme === "night" ? (
-        <Sun className="size-6" />
-      ) : (
-        <Moon className="size-6" />
-      )}
-    </button>
+    <div className="tooltip tooltip-bottom" data-tip="Theme">
+      <button
+        onClick={toggleTheme}
+        className="btn btn-ghost btn-square"
+        aria-label="Toggle Theme"
+      >
+        {theme === "night" ? (
+          <Sun className="size-6" />
+        ) : (
+          <Moon className="size-6" />
+        )}
+      </button>
+    </div>
   );
 }
