2f0be1a8ad35af5fb553211b06f0ad886327b4ff
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-08 22:16:35 +0530

    added train page

---
+++ app/routes/_train.train-prompt.tsx
@@ -0,0 +1,183 @@
+import { Bot } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import MessageBox from "~/component/chatBot/MessageBox";
+import Messages from "~/component/chatBot/Messages";
+import SideBar from "~/component/trainBot/SideDrawer";
+
+const data = [
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+];
+
+
+export default function TrainPrompt() {
+  const [history, setHistory] = useState(data);
+  const endRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    globalThis.scrollTo({ top: globalThis.document.body.scrollHeight });
+  }, []);
+
+  return (
+    <div>
+      <SideBar className="fixed left-0 lg:block hidden border-r border-base-content/50" />
+      <main className="lg:pl-75 p-5">
+        {history.length > 0 ? (
+          <div className="min-h-[calc(100dvh-95px)] max-w-5xl flex flex-col mx-auto gap-y-6">
+            <div className="fixed -z-10 inset-0 flex items-center justify-center lg:pl-75">
+              <figure className="h-52 w-52 bg-base-content/20 flex items-center justify-center rounded-full opacity-10 blur-[3px]">
+                <Bot className="size-30" />
+              </figure>
+            </div>
+            <Messages messages={[...history].reverse()} />
+            <section className="w-full sticky bottom-5">
+              <MessageBox />
+            </section>
+          </div>
+        ) : (
+          <div className="min-h-[calc(100dvh-95px)] flex items-center justify-center max-w-5xl text-center mx-auto -mt-6">
+            <div className="flex items-center justify-center flex-col w-full gap-y-2">
+              <figure className="w-40 h-40 bg-base-content/40 flex items-center justify-center rounded-full mb-1.5">
+                <Bot className="size-24" />
+              </figure>
+              <h1 className="font-extrabold text-4xl">Prompt 1.</h1>
+              <p className="max-w-[600px]">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore
+                quas aperiam dolor, tenetur modi sed aut natus sit itaque
+                deserunt perferendis, molestiae voluptatum necessitatibus nisi,
+                magni soluta explicabo. Ipsa, at!
+              </p>
+              <section className="w-full md:mt-10 mt-7">
+                <MessageBox />
+              </section>
+            </div>
+          </div>
+        )}
+        <div ref={endRef} id="end"></div>
+      </main>
+    </div>
+  );
+}
---
+++ app/routes/_train.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "@remix-run/react";
+import DashboardTrainNavbar from "~/component/trainBot/DashboardTrainNavbar";
+import TrainBotProvider from "~/context/TrainBot";
+
+export default function TrainLayout() {
+  return (
+    <>
+      <TrainBotProvider>
+        <DashboardTrainNavbar />
+        <div>
+          <Outlet />
+          <div className="fixed -bottom-10 left-10 bg-primary h-32 w-32 -z-10 blur-[120px]"></div>
+          <div className="fixed top-10 -right-10 bg-primary h-32 w-32 -z-10 blur-[160px]"></div>
+        </div>
+      </TrainBotProvider>
+    </>
+  );
+}
