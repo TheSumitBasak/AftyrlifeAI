be5addb24b56b26d560e83a24ce602ca63821c11
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-09 20:54:00 +0530

    added history

--- app/component/trainBot/TestPrompt.tsx
+++ app/component/trainBot/TestPrompt.tsx
@@ -1,13 +1,149 @@
 import { Bot, X } from "lucide-react";
-import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import Messages from "../chatBot/Messages";
 import MessageBox from "../chatBot/MessageBox";
 import { useTrainBotContext } from "~/context/TrainBot";
 
+const data = [
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+  {
+    role: "user",
+    message: "Hello!",
+  },
+  {
+    role: "model",
+    message: "Hello! How can i be your assitence!",
+  },
+  {
+    role: "user",
+    message: "Can you tell me a joke?",
+  },
+  {
+    role: "model",
+    message:
+      "Sure! Why don't scientists trust atoms? Because they make up everything!",
+  },
+  {
+    role: "user",
+    message: "That's funny!",
+  },
+  {
+    role: "model",
+    message: "I'm glad you liked it!",
+  },
+];
+
+
 export default function TestPrompt() {
   const { isOpen, setIsOpen } = useTrainBotContext();
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState(data);
   const endRef = useRef<HTMLDivElement>(null);
+  const chatRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (isOpen && chatRef.current) {
+      chatRef.current.scrollTo({ top: chatRef.current.scrollHeight });
+    }
+  }, [isOpen])
 
   return (
     <div className="drawer">
@@ -24,8 +160,10 @@ export default function TestPrompt() {
           aria-label="close sidebar"
           className="drawer-overlay"
         ></label>
-        <div className="h-[100dvh] overflow-auto w-[100vw] bg-base-200 max-w-5xl relative">
-          <div className="py-2.5 sticky top-0 border-b border-base-content/40 text-lg font-semibold px-5 flex justify-between items-center">
+        <div
+          ref={chatRef}
+          className="h-[100dvh] overflow-auto w-[100vw] bg-base-200 max-w-5xl relative">
+          <div className="bg-base-200/80 py-2.5 sticky top-0 z-[60] border-b border-base-content/40 text-lg font-semibold px-5 flex justify-between items-center">
             <span>Test prompt</span>
             <button
               onClick={() => setIsOpen(false)}
