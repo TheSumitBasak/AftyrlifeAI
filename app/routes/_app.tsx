1f74d4810543425b5bcbdf3a14f5d308e8d65d3d
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-09 20:41:22 +0530

    added footer

--- app/routes/_app.tsx
+++ app/routes/_app.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "@remix-run/react";
+import Footer from "~/component/Footer";
 import Navbar from "~/component/Navbar";
 
 export default function AppLayout() {
@@ -6,6 +7,7 @@ export default function AppLayout() {
     <>
       <Navbar />
       <Outlet />
+      <Footer/>
     </>
   );
 }
