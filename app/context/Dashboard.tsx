a6ba38a0da37874cd6f292cc5a0fba66b4a92464
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-11 22:03:46 +0530

    added get annd create prompt api

--- app/context/Dashboard.tsx
+++ app/context/Dashboard.tsx
@@ -1,4 +1,14 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import { useNavigate } from "@remix-run/react";
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { useCookies } from "react-cookie";
+import { useNotifications } from "reapop";
+import { getRequest, postRequest } from "~/utility/api";
 
 const DashboardContext = createContext<any>({});
 
@@ -6,8 +16,60 @@ export default function DashboardProvider({
   children,
 }: {
   children: Readonly<ReactNode>;
-  }) {
-  const value = {};
+}) {
+  const [cookies] = useCookies(["token"]);
+  const navigate = useNavigate();
+
+  const { notify } = useNotifications();
+
+  useEffect(() => {
+    if (!cookies.token) {
+      navigate("/login");
+      notify("Session expired", "info");
+    }
+  }, [cookies]);
+
+  const getPrompts = async (page = 1) => {
+    try {
+      const res = await getRequest(`/user-prompt?page=${page}`, cookies.token);
+      if (!res) {
+        notify("Internal server error", "error");
+        return false;
+      }
+      return res.data;
+    } catch (err: any) {
+      console.log(err);
+      notify(
+        err?.response?.data?.message || err.message || "Internal server error",
+        "error"
+      );
+    }
+  };
+
+  const createPrompt = async ({
+    name,
+    description,
+  }: {
+    name: String;
+    description: string;
+  }) => {
+    try {
+      const loginRes: any = await postRequest("/user-prompt", {
+        name,
+        description,
+      }, cookies?.token);
+
+      if (!loginRes?.data) {
+        return "Internal server error";
+      }
+      notify("Prompt created successfully", "success");
+      navigate("/train-prompt");
+    } catch (err: any) {
+      return err?.response?.data?.message || err.message;
+    }
+  };
+
+  const value = { getPrompts, createPrompt };
   return (
     <DashboardContext.Provider value={value}>
       {children}
