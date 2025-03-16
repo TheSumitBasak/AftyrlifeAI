8774efb93dd5471ad365f1e1cf5cfdfaa5af451c
TheSumitBasak <sumitbasak2208@gmail.com>
2025-03-16 20:15:11 +0530

    added test prompt feature

--- app/component/trainBot/SideDrawer.tsx
+++ app/component/trainBot/SideDrawer.tsx
@@ -1,8 +1,16 @@
 import { useParams } from "@remix-run/react";
-import { BookOpenCheck, Bot, Loader, SaveAll, Settings2 } from "lucide-react";
+import {
+  BookOpenCheck,
+  Bot,
+  Loader,
+  SaveAll,
+  Settings2,
+  Trash2,
+} from "lucide-react";
 import { ReactNode, useState } from "react";
 import { useDashboardContext } from "~/context/Dashboard";
 import { useTrainBotContext } from "~/context/TrainBot";
+import ConfirmationModal from "../ConfirmationModal";
 
 export default function SideBar({
   className = "",
@@ -11,8 +19,11 @@ export default function SideBar({
   className?: string;
   children?: Readonly<ReactNode>;
 }) {
-  const { prompt, savePromptMessage } = useDashboardContext();
+  const { prompt, savePromptMessage, deleteUserPrompt } = useDashboardContext();
   const { isOpen, setIsOpen } = useTrainBotContext();
+
+  const [isConfirmationDeleteOpen, setIsConfirmationDeleteOpen] =
+    useState(false);
 
   const params = useParams();
 
@@ -24,42 +35,60 @@ export default function SideBar({
     setIsLoading(false);
   };
   return (
-    <aside
-      className={`w-70 max-w-[90vw] lg:h-[calc(100dvh-54px)] h-[100dvh] bg-base-300 px-3 py-4 ${className}`}
-    >
-      {children}
-      <div className="flex flex-col items-center jusitfy-center pr-5 pt-2">
-        <figure className="w-20 h-20 bg-base-content/15 flex items-center justify-center rounded-full mb-1.5">
-          <Bot className="size-12" />
-        </figure>
-        <h3 className="text-xl font-bold">{prompt?.name || "Prompt 1."}</h3>
-      </div>
-      <ul className="space-y-3 mt-5 font-[350]">
-        <li className="w-full items-center flex justify-start gap-4 px-3 py-2 bg-base-200 hover:bg-base-100 cursor-pointer rounded-lg">
-          <Settings2 className="size-7" strokeWidth={1.2} />
-          Edit Metadata
-        </li>
-        <li
-          onClick={() => setIsOpen((op: boolean) => !op)}
-          className="w-full items-center flex justify-start gap-4 px-3 py-2 bg-base-200 hover:bg-base-100 cursor-pointer rounded-lg"
-        >
-          <BookOpenCheck className="size-7" strokeWidth={1.2} />
-          Test Prompt
-        </li>
-      </ul>
-      <button
-        onClick={onSubmit}
-        className="absolute bottom-4 btn btn-primary w-63 py-3"
+    <>
+      <aside
+        className={`w-70 max-w-[90vw] lg:h-[calc(100dvh-54px)] h-[100dvh] bg-base-300 px-3 py-4 ${className}`}
       >
-        {isLoading ? (
-          <Loader className="size-5 stroke-base-200 spin" />
-        ) : (
-          <>
-            <SaveAll className="mr-1" />
-            Save Prompt
-          </>
-        )}
-      </button>
-    </aside>
+        {children}
+        <div className="flex flex-col items-center jusitfy-center pr-5 pt-2">
+          <figure className="w-20 h-20 bg-base-content/15 flex items-center justify-center rounded-full mb-1.5">
+            <Bot className="size-12" />
+          </figure>
+          <h3 className="text-xl font-bold">{prompt?.name || "Prompt 1."}</h3>
+        </div>
+        <ul className="space-y-3 mt-5 font-[350]">
+          <li className="w-full items-center flex justify-start gap-4 px-3 py-2 bg-base-200 hover:bg-base-100 cursor-pointer rounded-lg">
+            <Settings2 className="size-7" strokeWidth={1.2} />
+            Edit Metadata
+          </li>
+          <li
+            onClick={() => setIsOpen((op: boolean) => !op)}
+            className="w-full items-center flex justify-start gap-4 px-3 py-2 bg-base-200 hover:bg-base-100 cursor-pointer rounded-lg"
+          >
+            <BookOpenCheck className="size-7" strokeWidth={1.2} />
+            Test Prompt
+          </li>
+          <li
+            onClick={() => setIsConfirmationDeleteOpen(true)}
+            className="w-full items-center flex justify-start gap-4 px-3 py-2 bg-base-200 hover:bg-base-100 cursor-pointer rounded-lg"
+          >
+            <Trash2 className="size-7" strokeWidth={1.2} />
+            Delete Prompt
+          </li>
+        </ul>
+        <button
+          onClick={onSubmit}
+          className="absolute bottom-4 btn btn-primary w-63 py-3"
+        >
+          {isLoading ? (
+            <Loader className="size-5 stroke-base-200 spin" />
+          ) : (
+            <>
+              <SaveAll className="mr-1" />
+              Save Prompt
+            </>
+          )}
+        </button>
+      </aside>
+      <ConfirmationModal
+        isOpen={isConfirmationDeleteOpen}
+        onOk={async() => {await deleteUserPrompt({ promptId: params.chatId });}}
+        title="Confirmation"
+        message={
+          "Are you sure you want to delete this prompt?\nThis action cannot be undone."
+        }
+        onClose={() => setIsConfirmationDeleteOpen(false)}
+      />
+    </>
   );
 }
--- app/component/trainBot/TestPrompt.tsx
+++ app/component/trainBot/TestPrompt.tsx
@@ -3,6 +3,10 @@ import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import Messages from "../chatBot/Messages";
 import MessageBox from "../chatBot/MessageBox";
 import { useTrainBotContext } from "~/context/TrainBot";
+import { useDashboardContext } from "~/context/Dashboard";
+import { useNavigate, useParams } from "@remix-run/react";
+import { Message } from "~/types/message";
+import { Prompt } from "~/types/prompt";
 
 const data = [
   {
@@ -132,18 +136,106 @@ const data = [
   },
 ];
 
-
-export default function TestPrompt() {
+export default function TestPrompt({
+  prompt,
+  msgLength,
+}: {
+  prompt: Prompt;
+  msgLength: number;
+}) {
   const { isOpen, setIsOpen } = useTrainBotContext();
-  const [history, setHistory] = useState(data);
-  const endRef = useRef<HTMLDivElement>(null);
   const chatRef = useRef<HTMLDivElement>(null);
+
+  const { getTrainMessages, sendTrainMessage, resetChatMessage } =
+    useDashboardContext();
+
+  const [history, setHistory] = useState<Message[]>([]);
+  const endRef = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
+
+  const params = useParams();
+
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalRecords, setTotalRecords] = useState(0);
+  const [currentPage, setcurrentPage] = useState(1);
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMsgLoading, setMsgIsLoading] = useState(false);
+
+  const [isNewMsg, setIsNewMsg] = useState(true);
 
   useEffect(() => {
     if (isOpen && chatRef.current) {
       chatRef.current.scrollTo({ top: chatRef.current.scrollHeight });
     }
-  }, [isOpen])
+  }, [isOpen, history]);
+
+  useEffect(() => {
+    if (msgLength > 0) getMessages();
+  }, [msgLength]);
+
+  const getMessages = async (page = 1) => {
+    const prevScrollHeight = document.body.scrollHeight;
+    const messageRes = await getTrainMessages({
+      page,
+      promptId: params.chatId,
+    });
+    if (messageRes) {
+      setTotalPages(messageRes?.metadata?.totalPage || 1);
+      setTotalRecords(messageRes?.metadata?.totalRecords || 0);
+      setcurrentPage(page);
+      if (page == 1) {
+        setIsNewMsg(true);
+        setHistory(messageRes?.data || []);
+      } else {
+        setHistory((pr) => {
+          const newMessages = messageRes?.data || [];
+          const uniqueMessages = newMessages.filter(
+            (msg: Message) =>
+              !pr.some((existingMsg) => existingMsg.message === msg.message)
+          );
+          return [...pr, ...uniqueMessages];
+        });
+        const newScrollHeight = document.body.scrollHeight;
+        window.scrollTo(
+          0,
+          window.scrollY + (newScrollHeight - prevScrollHeight)
+        );
+      }
+    }
+    setIsLoading(false);
+  };
+
+  const onSubmit = async (text: string) => {
+    setMsgIsLoading(true);
+    setIsNewMsg(true);
+    setHistory((pr) => [
+      {
+        role: "user",
+        message: text,
+        _id: "Demo",
+        createdAt: new Date().toISOString(),
+      },
+      ...pr,
+    ]);
+    const msgRes = await sendTrainMessage({
+      message: text,
+      promptId: params.chatId,
+    });
+    if (msgRes?.message) {
+      setIsNewMsg(true);
+      setHistory((pr) => [
+        {
+          role: "model",
+          message: msgRes?.message,
+          _id: "Demo",
+          createdAt: new Date().toISOString(),
+        },
+        ...pr,
+      ]);
+    }
+    setMsgIsLoading(false);
+  };
 
   return (
     <div className="drawer">
@@ -162,7 +254,8 @@ export default function TestPrompt() {
         ></label>
         <div
           ref={chatRef}
-          className="h-[100dvh] overflow-auto w-[100vw] bg-base-200 max-w-5xl relative">
+          className="h-[100dvh] overflow-auto w-[100vw] bg-base-200 max-w-5xl relative"
+        >
           <div className="bg-base-200/80 py-2.5 sticky top-0 z-[60] border-b border-base-content/40 text-lg font-semibold px-5 flex justify-between items-center">
             <span>Test prompt</span>
             <button
@@ -178,9 +271,15 @@ export default function TestPrompt() {
             </figure>
           </div>
           <div className="flex flex-col gap-y-6 min-h-[92%] px-5">
-            <Messages messages={[...history].reverse()} />
+            <Messages
+              hasMore={currentPage != totalPages}
+              messages={history}
+              next={async () => getMessages(currentPage + 1)}
+              prompt={prompt}
+              isLoading={isMsgLoading}
+            />
             <section className="w-full sticky bottom-5">
-              <MessageBox />
+              <MessageBox onSubmit={onSubmit} />
             </section>
             <div ref={endRef} id="end"></div>
           </div>
